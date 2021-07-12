db3d70ec8976238cb6ba2a282ab57cc070d4deb6
adam <adamdong@phas.ubc.ca>
2021-07-12 12:40:27 -0700

    commiting automated dspsr stuff

--- automated_dspsr.sh
+++ automated_dspsr.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
+#this serves as the automated dspsr program
+dspsr -c 0.4 -N J0012+54 -s -S 627.6 -T 0.4 -k chime J0012+54_59358_pow.fil
+#need to read the zapchan file
---
+++ read_zapchan.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+import sys
+
+def convert_to_presto
