232c227899764e6bc717cc47e81c1e9f0960617a
adamdong <adamdong@phas.ubc.ca>
2021-07-12 13:30:35 -0700

    zap chan conversion script

--- read_zapchan.py
+++ read_zapchan.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 import sys
+import numpy as np
 
-def convert_to_presto
+
+def convert_to_presto(fn):
+    #
+    #fn is the file name to read
+    #convert presto zapfile
+    #
+    with open(fn,'r') as zapfile:
+        channels = zapfile.readlines()
+        chans = (int(channel) for channel in channels)
+        psrchive_chans = np.arange(0, 1024)[::-1]
+        psrchive_chans = psrchive_chans[chans]
+        chan_str = ",".join(psrchive_chans.astype(str))
+    return chan_str
+
+
+print(convert_to_presto(sys.argv[1]))
