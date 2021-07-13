9a7d5e73ae59ac66bc09906607aba448276c8f6e
adamdong <adamdong@phas.ubc.ca>
2021-07-13 16:07:18 -0700

    ishould work now

--- get_pulses.py
+++ get_pulses.py
@@ -26,10 +26,17 @@ with open('extracted_bursts.csv','w') as csv_file:
                 # first gotta find the file and load up filterbank
                 fb_file = '%s_%s_pow.fil'%(basename,key)
                 fb_file = FilterbankFile(fb_file)
-                import pdb; pdb.set_trace()
                 mjd_pulse = fb_file.tstart+float(burst[0])/60/60/24
+                obs_length = fb_file.nspec*fb_file.dt
+                writer.writerow([mjd_pulse,obs_length])
         else:
-            writer.writerow([key,burst_dict[key][0]])
+            # writer.writerow([key,burst_dict[key][0]])
+            fb_file = '%s_%s_pow.fil'%(basename,key)
+            fb_file = FilterbankFile(fb_file)
+            mjd_pulse = fb_file.tstart+float(burst[key][0])/60/60/24
+            obs_length = fb_file.nspec*fb_file.dt
+            writer.writerow([mjd_pulse,obs_length])
+
 
 
 #multiday_times = pt.build_multiday_from_dict(burst_dict,min_day=1,min_time=0,sigma=3)
