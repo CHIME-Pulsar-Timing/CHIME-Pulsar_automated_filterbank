7cb1a64629df93607f15ad2b1b5d43242f7829cf
Kathryn Crowter <kcrowter@phas.ubc.ca>
2021-10-27 16:28:42 -0700

    change SLURM_TMPDIR and file delete/copy for testing

--- automated_filterbank_batch.sh
+++ automated_filterbank_batch.sh
@@ -25,6 +25,9 @@ AFP=$4
 #check that the filterbank file exists this prevents accidental deletion of files with the later rm command
 #********************THIS IS THE LAZY WAY OUT!!!
 #PULSAR=$(echo "$3" | cut -f 1 -d '.')
+# KC will need to figure out the file movement better but for now:
+FN=S{3%.fil}
+SLURM_TMPDIR="/home/kcrowter/scratch/survey/$FN"
 #SLURM_TMPDIR='/home/adamdong/scratch/tmpdir/'$PULSAR
 #SLURM_TMPDIR='/media/adam/1c126a4b-fb16-4471-909f-4b0fda74a5d2/tmpdir/'$PULSAR
 #mkdir -p $SLURM_TMPDIR
@@ -67,17 +70,18 @@ if test -f "$3"; then
 			   cp -r -d ${SLURM_TMPDIR}/* $ERRORS
 			exit 1
 		done
+        # KC commented out for testing, uncomment later
         #remove the extra fil files
-        rm "$SPFILES/$FIL"
-        rm "$SPFILES/"*sk_mad.fil
+        #rm "$SPFILES/$FIL"
+        #rm "$SPFILES/"*sk_mad.fil
         #tarball the .dat files
-        tar -czf "$SPFILES/${FIL}_dat.tar" -C "$SPFILES" *.dat
+        #tar -czf "$SPFILES/${FIL}_dat.tar" -C "$SPFILES" *.dat
         #tarball the infs and singlepulse files
-        tar -czf "$SPFILES/${FIL}_singlepulse.tar" -C "$SPFILES" *.singlepulse
-        tar -cff "$SPFILES/${FIL}_inf.tar" -C "$SPFILES" *DM*.inf
-        rm "$SPFILES"/*DM*.inf
-        rm "$SPFILES"/*DM*.singlepulse
-        rm "$SPFILES"/*.dat
+        #tar -czf "$SPFILES/${FIL}_singlepulse.tar" -C "$SPFILES" *.singlepulse
+        #tar -cff "$SPFILES/${FIL}_inf.tar" -C "$SPFILES" *DM*.inf
+        #rm "$SPFILES"/*DM*.inf
+        #rm "$SPFILES"/*DM*.singlepulse
+        #rm "$SPFILES"/*.dat
         ((i=i+1))
     done
     #uncomment this code if you want to make a folder and shove everything in there, if you're using process_all_fil.sh, it already makes folder for you.
