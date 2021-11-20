b7f947c83608532c72598ab141ad4ef0b3e6d920
adam <adamdong@phas.ubc.ca>
2021-11-20 07:04:02 -0800

    added the -b flag to single pulse search

--- gwg_cand_search_pipeline.py
+++ gwg_cand_search_pipeline.py
@@ -126,7 +126,7 @@ def run_ffa(fname):
         run_ffa_cmd.wait()
 
 def run_sp(fname):
-    sp_command = 'single_pulse_search.py %s*.dat' %(fname)
+    sp_command = 'single_pulse_search.py -b %s*.dat' %(fname)
     failed=True
     try:
         run_sp_cmd = subprocess.check_call([sp_command],shell=True)
