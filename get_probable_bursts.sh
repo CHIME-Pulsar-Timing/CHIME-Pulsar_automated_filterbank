45bb5a74d0c144d8efdb6e61c6c3a872b0f6360d
adam <adamdong@phas.ubc.ca>
2022-01-03 13:25:28 -0800

    commiting chnages

--- get_probable_bursts.sh
+++ get_probable_bursts.sh
@@ -16,14 +16,16 @@ do
     RESULT_FILE="$RESULT_PATH/results_a.csv"
     while IFS=, read -r cand filepath probability score
     do
-        if [[ $probability != "candidate" ]]; then
+        if [[ "$probability" != "probability" ]]; then
             ROOT=$(dirname $RESULT_PATH)
-            echo $ROOT
             if [[ "$probability" != *"e"* ]]; then
                 CAND_PATH=$ROOT/$filepath
                 CAND_PATH=$(echo "$CAND_PATH" | sed 's/.h5//')
                 PATH_PNG=$CAND_PATH.png
                 cp $PATH_PNG probable_bursts
+		echo "copying path:"
+		echo $PATH_PNG
+		echo $probability
                 echo "$CAND_PATH,$probability,$score" >> probable_bursts.csv
             fi
         fi
