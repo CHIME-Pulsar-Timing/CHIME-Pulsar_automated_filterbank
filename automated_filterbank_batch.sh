a7b6a6f4d6a064ea16d9f00909856debad98c81c
adam <adamdong@phas.ubc.ca>
2023-01-21 10:22:37 -0800

    modification

--- automated_filterbank_batch.sh
+++ automated_filterbank_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=rrg-istairs-ad
 #SBATCH --export=NONE
-#SBATCH --time=20:00:00
+#SBATCH --time=70:00:00
 #SBATCH --mem=20GB
 #SBATCH --cpus-per-task=10
 #SBATCH --job-name=automated_filterbank
@@ -57,7 +57,6 @@ if test -f "$p"; then
             DEAD_GPU=""
             echo "python $AFP/gwg_cand_search_pipeline.py --dm $DM --speg --fetch --rfifind --dedisp --sp --fil $FIL --slurm "${SLURM_TMPDIR}" && break"
             python $AFP/gwg_cand_search_pipeline.py --dm $DM --speg --fetch --rfifind --sk_mask --dedisp --sp --fil $FIL --slurm "${SLURM_TMPDIR}" && break
-
         else
             DEAD_GPU=$(get_bad_channel_list.py --fmt presto --type filterbank $FIL)
             echo "python $AFP/gwg_cand_search_pipeline.py --dm $DM --speg --fetch --rfifind --dead_gpu $DEAD_GPU --dedisp --sp --fil $FIL --slurm "${SLURM_TMPDIR}" && break"
@@ -73,13 +72,13 @@ if test -f "$p"; then
         n=$((n+1))
         # sleep 15
         #if it fails, lets copy all the things to my scratch directory then exit with error code
-        # PULSAR=$(echo "$FIL" | cut -f 1 -d '.')
-        #ERRORS=~/"scratch/errors/${PULSAR}_${SLURM_JOB_ID}"
-        #echo "copying error files to ${ERRORS}"
-        #df -h
-        #mkdir -p $ERRORS
-        #   cp -r -d ${SLURM_TMPDIR}/* $ERRORS
-        #exit 1
+        PULSAR=$(echo "$FIL" | cut -f 1 -d '.')
+        ERRORS=~/"scratch/errors/${PULSAR}_${SLURM_JOB_ID}"
+        echo "copying error files to ${ERRORS}"
+        df -h
+        mkdir -p $ERRORS
+        cp -r -d ${SLURM_TMPDIR}/* $ERRORS
+        exit 1
         #remove the extra fil files
     done
     #run candmaker
