50ebdd8fc730e33a2f06e87d57ae0955ad79d12b
adam <adamdong@phas.ubc.ca>
2023-05-02 06:31:17 -0700

    time changes

--- automated_filterbank_batch.sh
+++ automated_filterbank_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=rrg-istairs-ad
 #SBATCH --export=NONE
-#SBATCH --time=10:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=16GB
 #SBATCH --ntasks=1
 #SBATCH --job-name=automated_filterbank
