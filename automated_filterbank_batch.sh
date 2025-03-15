2751375e104a394da7ac49c8dafba8c4ee2dbd2c
fengqiu.dong <fengqiu.dong@proton.me>
2025-03-15 16:09:52 -0400

    updates

--- automated_filterbank_batch.sh
+++ automated_filterbank_batch.sh
@@ -13,18 +13,12 @@ EXT="${p##*.}"
 #source the pulsar environtment
 source /home/pulsar_rhel8/pulsar.bash
 #make sure that $p is a file
-if test -f "$p"; then
-    #rename it FIL
-    FIL=$p
-    echo $EXT
-    if [ $EXT == "fits" ]; then
-        python $AFP/gwg_cand_search_pipeline.py --dm $DM --rfifind --sk_mask --kc_iqrm --dedisp --fil $FIL
-    else
+FIL=$p
+echo $EXT
+if [ $EXT == "fits" ]; then
+    python $AFP/gwg_cand_search_pipeline.py --dm $DM --rfifind --sk_mask --kc_iqrm --dedisp --fil $FIL
+else
 
-    PULSAR=$(echo "$FIL" | cut -f 1 -d '.')
-    echo "ALL DONE"
-    exit 0
-fi
-#didn't find file, throw error
-echo "filterbank file not found"
-exit 1
+PULSAR=$(echo "$FIL" | cut -f 1 -d '.')
+echo "ALL DONE"
+exit 0
