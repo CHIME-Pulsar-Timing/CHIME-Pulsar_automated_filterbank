6c35180463f64ac8e479cb5e36fff106d8f5a748
fengqiu.dong <fengqiu.dong@proton.me>
2025-03-15 16:14:32 -0400

    remove force

--- gwg_cand_search_pipeline.py
+++ gwg_cand_search_pipeline.py
@@ -235,7 +235,6 @@ if __name__ == '__main__':
         format="%(asctime)s %(levelname)s:%(message)s",
         datefmt="%d-%b-%y %H:%M:%S",
         level=logging.DEBUG,
-        force=True
     )
     if slurm:
         #this is a change to the SLURM tmpdir directory
