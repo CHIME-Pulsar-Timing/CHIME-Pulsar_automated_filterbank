0558de860f3990d508b37021c080e91aa1d47d45
fengqiu.dong <fengqiu.dong@proton.me>
2025-04-04 08:14:31 -0400

    modifying to be dynamic

--- rfi_and_dedisperse.sh
+++ rfi_and_dedisperse.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 for file in "$@"; do
-    python ~/Documents/CHIME-Pulsar_automated_filterbank/gwg_cand_search_pipeline.py --sk_mask --kc_iqrm --rfifind --dedisp --dm 82.5 --one_dm --fil $file &
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+    echo $SCRIPT_DIR
+    python $SCRIPT_DIR/gwg_cand_search_pipeline.py --sk_mask --kc_iqrm --rfifind --dedisp --dm 82.5 --one_dm --fil $file &
 done
